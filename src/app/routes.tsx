6aa41d8ae7e7e43475334dc38c8d26641297d83d
carluks78 <taxingofb@gmail.com>
2026-05-18 13:06:41 +0200

    Fix duplicate route entry for taxi-gare-narbonne

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -11,7 +11,7 @@ export default [
     route('taxi-narbonne', './pages/TaxiNarbonne.tsx'),
     route('reserver-taxi-narbonne', './pages/Reservation.tsx'),
 
-    route('taxi-gare-narbonne', './pages/TaxiGare.tsx')
+    route('taxi-gare-narbonne', './pages/TaxiGare.tsx'),
 
     route('taxi-aeroport-montpellier', './pages/AirportPage.tsx'),
     route('taxi-aeroport-toulouse', './pages/AirportPage.tsx'),
