39cd79247248b9dd08b205ed96062ca6b5412106
carluks78 <taxingofb@gmail.com>
2026-05-18 13:39:08 +0200

    Remove unused imports from Layout component

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,15 +1,14 @@
-import { Outlet, useLocation } from "react-router";
-import { useEffect } from "react";
-
+import { Outlet } from "react-router";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { FloatingWidgets } from "./FloatingWidgets";
 
 export default function Layout() {
-  const { pathname } = useLocation();
-
   return (
-    <div className="min-h-screen flex flex-col" style={{ backgroundColor: "#060F1E" }}>
+    <div
+      className="min-h-screen flex flex-col"
+      style={{ backgroundColor: "#060F1E" }}
+    >
       <Header />
 
       <main className="flex-1">
