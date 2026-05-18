27fcc6fd4bca7e5c0cf346d672e35538db7b9a5a
carluks78 <taxingofb@gmail.com>
2026-05-18 12:32:42 +0200

    Change loader return value from empty object to null

--- src/app/pages/Services.tsx
+++ src/app/pages/Services.tsx
@@ -4,7 +4,7 @@ import { Phone, Train, Heart, Plane, Route, Users, Car, CheckCircle } from 'luci
 import { SEOHead } from '../components/SEOHead';
 
 export async function loader() {
-  return {};
+  return null;
 }
 
 const PHONE = '0768303303';
