8bcd59003fcd1735d05e31c80b5c3de5f575d2bb
carluks78 <taxingofb@gmail.com>
2026-05-18 12:49:19 +0200

    Add GeoPage component with test heading

--- src/app/pages/GeoPage.tsx
+++ src/app/pages/GeoPage.tsx
@@ -11,6 +11,10 @@ import {
 import { useState } from 'react';
 import { SEOHead } from '../components/SEOHead';
 
+export default function GeoPage() {
+  return <h1 style={{ color: 'white' }}>TEST GEO OK</h1>;
+}
+
 const PHONE = '0768303303';
 const PHONE_DISPLAY = '07 68 30 33 03';
 const TEAL = '#3AB4B1';
