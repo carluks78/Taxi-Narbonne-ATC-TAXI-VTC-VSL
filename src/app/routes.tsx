7fa88602094244ddabca49c74bd933824f74f89e
carluks78 <taxingofb@gmail.com>
2026-05-18 12:15:57 +0200

    Update route layout component path

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,7 +1,8 @@
 import { type RouteConfig, index, route } from '@react-router/dev/routes'
 
 export default [
-  route('/', './components/Layout.tsx', [
+  export default [
+  route('/', './pages/_layout.tsx', [
 
     index('./pages/Home.tsx'),
     route('services',                './pages/Services.tsx'),
