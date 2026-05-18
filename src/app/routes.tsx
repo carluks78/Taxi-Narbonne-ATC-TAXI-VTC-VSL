6fda2338a0c24bad885ebae1382537d89fab20f5
carluks78 <taxingofb@gmail.com>
2026-05-18 11:59:27 +0200

    Change layout component path in routes configuration

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,7 +1,7 @@
 import { type RouteConfig, index, route } from '@react-router/dev/routes'
 
 export default [
-  route('/', './pages/_layout.tsx', [
+  route('/', './components/Layout.tsx', [
 
     index('./pages/Home.tsx'),
     route('services',                './pages/Services.tsx'),
