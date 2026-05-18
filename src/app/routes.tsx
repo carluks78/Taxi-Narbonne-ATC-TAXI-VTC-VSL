361021887d0f60ac42469438157342d6ef7c8569
carluks78 <taxingofb@gmail.com>
2026-05-18 11:56:32 +0200

    Update route configuration to use _layout.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,7 +1,7 @@
 import { type RouteConfig, index, route } from '@react-router/dev/routes'
 
 export default [
-  route('/', './components/Layout.tsx', [
+  route('/', './pages/_layout.tsx', [
 
     index('./pages/Home.tsx'),
     route('services',                './pages/Services.tsx'),
