597e6789fa65d18a61ca45879f17e8a7f8f32689
carluks78 <taxingofb@gmail.com>
2026-05-18 13:27:26 +0200

    Refactor routes and update layout component

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,36 +1,16 @@
-import { type RouteConfig, index, route } from '@react-router/dev/routes'
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  route('/', './components/Layout.tsx', [
-    index('./pages/Home.tsx'),
+  route("", "./routes/_layout.tsx", [
+    index("./pages/Home.tsx"),
 
-    route('services', './pages/Services.tsx'),
-    route('contact', './pages/Contact.tsx'),
-    route('zones-desservies', './pages/ZonesDesservies.tsx'),
-    route('avis-clients', './pages/AvisClients.tsx'),
-    route('taxi-narbonne', './pages/TaxiNarbonne.tsx'),
-    route('reserver-taxi-narbonne', './pages/Reservation.tsx'),
+    route("services", "./pages/Services.tsx"),
+    route("contact", "./pages/Contact.tsx"),
+    route("zones-desservies", "./pages/ZonesDesservies.tsx"),
+    route("avis-clients", "./pages/AvisClients.tsx"),
+    route("taxi-narbonne", "./pages/TaxiNarbonne.tsx"),
+    route("reserver-taxi-narbonne", "./pages/Reservation.tsx"),
 
-    // 🚖 GARE
-    route('taxi-gare-narbonne', './pages/gare/Narbonne.tsx'),
-    route('taxi-gare-de-narbonne', './pages/gare/NarbonneDe.tsx'),
-
-    // ✈️ AÉROPORTS
-    route('taxi-aeroport-montpellier', './pages/aeroport/Montpellier.tsx'),
-    route('taxi-aeroport-toulouse', './pages/aeroport/Toulouse.tsx'),
-    route('taxi-aeroport-carcassonne', './pages/aeroport/Carcassonne.tsx'),
-    route('taxi-aeroport-beziers', './pages/aeroport/Beziers.tsx'),
-    route('taxi-aeroport-barcelone', './pages/aeroport/Barcelone.tsx'),
-
-    // 📍 ZONES
-    route('taxi-gruissan', './pages/geo/Gruissan.tsx'),
-    route('taxi-leucate', './pages/geo/Leucate.tsx'),
-    route('taxi-sigean', './pages/geo/Sigean.tsx'),
-    route('taxi-narbonne-plage', './pages/geo/NarbonnePlage.tsx'),
-    route('taxi-bages', './pages/geo/Bages.tsx'),
-    route('taxi-vinassan', './pages/geo/Vinassan.tsx'),
-    route('taxi-carcassonne', './pages/geo/Carcassonne.tsx'),
-
-    route('*', './pages/NotFound.tsx'),
+    route("*", "./pages/NotFound.tsx"),
   ]),
-] satisfies RouteConfig
+];
