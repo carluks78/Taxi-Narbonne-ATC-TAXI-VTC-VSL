44b9606e7d2b12cb195e81bdc48e98fef4061742
carluks78 <taxingofb@gmail.com>
2026-05-18 11:15:39 +0200

    Modify scroll behavior and set background color
    Update scroll behavior and background color in Layout component.

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet, useLocation } from "react-router";
 import { useEffect } from "react";
+
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { FloatingWidgets } from "./FloatingWidgets";
@@ -7,12 +8,16 @@ import { FloatingWidgets } from "./FloatingWidgets";
 export function Layout() {
   const { pathname } = useLocation();
 
+  // 🔥 toujours remonter en haut quand tu changes de page
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      behavior: "instant",
+    });
   }, [pathname]);
 
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col" style={{ backgroundColor: "#060F1E" }}>
       <Header />
 
       <main className="flex-1">
