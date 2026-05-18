7cd723d9336fd2319d8ed69e2cebab5063ef6393
carluks78 <taxingofb@gmail.com>
2026-05-18 13:26:52 +0200

    Add Layout component with header and footer

---
+++ src/routes/_layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+
+export default function Layout() {
+  return (
+    <>
+      <header>...</header>
+
+      <Outlet />
+
+      <footer>...</footer>
+    </>
+  );
+}
