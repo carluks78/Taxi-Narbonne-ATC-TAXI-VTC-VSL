7c0090fb9a7c84315cbb4e9997ae330b55cd13b8
carluks78 <taxingofb@gmail.com>
2026-05-18 13:37:24 +0200

    Refactor Layout component to RouteLayout

--- src/app/routes/_layout.tsx
+++ src/app/routes/_layout.tsx
@@ -1,15 +1,5 @@
-import { Outlet } from "react-router";
+import Layout from "../components/Layout";
 
-export default function Layout() {
-  return (
-    <>
-      {/* HEADER */}
-      <header>Ton header ici</header>
-
-      <Outlet />
-
-      {/* FOOTER */}
-      <footer>Ton footer ici</footer>
-    </>
-  );
+export default function RouteLayout() {
+  return <Layout />;
 }
