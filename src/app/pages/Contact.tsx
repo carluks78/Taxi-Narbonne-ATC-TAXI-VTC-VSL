133a0d67d2656d57282e5176109d61cfbbdde89f
carluks78 <taxingofb@gmail.com>
2026-05-18 10:16:00 +0200

    Add loader function to Contact page

--- src/app/pages/Contact.tsx
+++ src/app/pages/Contact.tsx
@@ -4,6 +4,10 @@ import { Link } from 'react-router';
 import { Phone, Mail, MapPin, Clock, MessageCircle, Send } from 'lucide-react';
 import { SEOHead } from '../components/SEOHead';
 
+export async function loader() {
+  return {};
+}
+
 const PHONE = '0768303303';
 const PHONE_DISPLAY = '07 68 30 33 03';
 const EMAIL = 'atctaxi11@gmail.com';
