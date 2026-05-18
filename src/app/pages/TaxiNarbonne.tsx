471d4e31a5adcc4225bc879f98778b71f32afe71
carluks78 <taxingofb@gmail.com>
2026-05-18 10:17:27 +0200

    Update TaxiNarbonne.tsx

--- src/app/pages/TaxiNarbonne.tsx
+++ src/app/pages/TaxiNarbonne.tsx
@@ -4,6 +4,10 @@ import { Phone, MessageCircle, CheckCircle, MapPin, Star, ChevronDown, ChevronUp
 import { useState } from 'react';
 import { SEOHead } from '../components/SEOHead';
 
+export async function loader() {
+  return {};
+}
+
 const PHONE = '0768303303';
 const PHONE_DISPLAY = '07 68 30 33 03';
 const WHATSAPP = 'https://wa.me/33768303303';
