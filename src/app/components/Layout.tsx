f426115b636e3046c4fbc38e333e2cf756670b67
carluks78 <taxingofb@gmail.com>
2026-05-18 11:12:27 +0200

    Refactor Layout component to remove inline styles
    Removed inline style for background color in Layout component.

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,8 +1,8 @@
-import { Outlet, useLocation } from 'react-router';
-import { useEffect } from 'react';
-import { Header } from './Header';
-import { Footer } from './Footer';
-import { FloatingWidgets } from './FloatingWidgets';
+import { Outlet, useLocation } from "react-router";
+import { useEffect } from "react";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+import { FloatingWidgets } from "./FloatingWidgets";
 
 export function Layout() {
   const { pathname } = useLocation();
@@ -12,11 +12,13 @@ export function Layout() {
   }, [pathname]);
 
   return (
-    <div className="min-h-screen flex flex-col" style={{ backgroundColor: '#060F1E' }}>
+    <div className="min-h-screen flex flex-col">
       <Header />
+
       <main className="flex-1">
         <Outlet />
       </main>
+
       <Footer />
       <FloatingWidgets />
     </div>
