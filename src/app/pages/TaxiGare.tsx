e07d186affcc07b9bed41d576b40f05f3ee2ba1b
carluks78 <taxingofb@gmail.com>
2026-05-18 12:32:52 +0200

    Update TaxiGare.tsx

--- src/app/pages/TaxiGare.tsx
+++ src/app/pages/TaxiGare.tsx
@@ -4,7 +4,7 @@ import { Phone, MessageCircle, Train, Clock, CheckCircle, Star } from 'lucide-re
 import { SEOHead } from '../components/SEOHead';
 
 export async function loader() {
-  return {};
+  return null;
 }
 
 const PHONE = '0768303303';
